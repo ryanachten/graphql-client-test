52d4d9d3e8d0b44152d7c13f06ec8fe10e358dfe
RyanAchten <ryanachten@gmail.com>
2018-12-23 11:00:50 +1300

    Render songs from gql and handle loading in SongList

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -3,15 +3,23 @@ import React from "react";
 import { graphql } from "react-apollo";
 
 class SongList extends React.Component {
+  renderSongs() {
+    const songs = this.props.data.songs;
+    if (!songs) return null;
+    return songs.map(song => <li key={song.id}>{song.title}</li>);
+  }
+
   render() {
-    console.log(this.props);
-    return <div>SongList</div>;
+    const loading = this.props.data.loading;
+    if (loading) return <div>Loading...</div>;
+    return <ul>{this.renderSongs()}</ul>;
   }
 }
 
 const query = gql`
   {
     songs {
+      id
       title
     }
   }
